d03b41a9addca4c79e3980d2dacae2d5ca0dade4
Filipe de Sousa Botelho <filipegame07@gmail.com>
2019-08-18 18:03:22 -0300

    configurar pm2 para o heroku

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const app = require('./server');
 const config = require('./config');
 
 // Iniciar servidor
-app.listen(config.PORT);
+app.listen(process.env.PORT || config.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT || config.PORT}`);
+});
